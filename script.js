53513cd81d29c4c5085ceaa0bbba00ef86474d49
JordanLindley <jlindley94@gmail.com>
2022-09-19 14:21:15 -0400

    Create playRound function to take playerChoice and compare to ComputerChoice

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 
 // getComputerChoice: 
-  // 1. create variable computerChoice that equals the output of getComputerChoice() 
+  // 1. create function getComputerChoice to randomly return either 'Rock', 'Paper', or 'Scissors'.
 function getComputerChoice() {
-  const computerSelections = ['Rock', 'Paper', 'Scissors'];
+  const computerSelections = ['rock', 'paper', 'scissors'];
   let random = Math.floor(Math.random()*(2-0+1)+0);
   return computerSelections[random];
 }
-  // 2. Randomly return either 'Rock', 'Paper', or 'Scissors'
- let computerChoice = getComputerChoice();
+  // 2. Assign returned value of getComputerChoice() to variable computerChoice.
 
-// getPlayerChoice:
-  // Takes input from player (must be either 'Rock', 'Paper', or 'Scissors')
-  // Must be case insensitive
+  // getPlayerChoice:
+let playerChoice = 'rock';
 
 // playRound:
   // Takes two parameters: computerChoice and playerChoice
@@ -19,5 +17,36 @@ function getComputerChoice() {
     // `You lose! ${} beats ${}!`
     // `You win! ${} beats ${}!`
   // Return results, do not console.log them.
+function playRound(computerChoice, playerChoice) {
+  playerChoice = prompt('Rock, Paper, Scissors, Shoot!', playerChoice);
+  computerChoice = getComputerChoice();
+
+  let youWin = `You win! ${playerChoice} beats ${computerChoice}.`;
+  let youLose = `You lose! ${computerChoice} beats ${playerChoice}`;
+  let tie = 'It\'s a tie! Play again!'
+
+  // Must be case insensitive
+  if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors') {
+    console.log(`Player Choice: ${playerChoice}`);
+    console.log(`Computer Choice: ${computerChoice}`);
+    return youWin;
+  } else if (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper') {
+    console.log(`Player Choice: ${playerChoice}`);
+    console.log(`Computer Choice: ${computerChoice}`);
+    return youWin;
+  } else if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') {
+    console.log(`Player Choice: ${playerChoice}`);
+    console.log(`Computer Choice: ${computerChoice}`);
+    return youWin;
+  } else if (playerChoice.toLowerCase() === computerChoice) {
+    console.log(`Player Choice: ${playerChoice}`);
+    console.log(`Computer Choice: ${computerChoice}`);
+    return tie;
+  } else if (playerChoice.toLowerCase() !== 'rock' || playerChoice.toLowerCase() !== 'paper' || playerChoice.toLowerCase() !== 'scissors')  {
+    return 'You must select "rock", "paper", or "scissors"';
+  } else return youLose;
+}
+
+
 
   // game() function = 5 rounds.
