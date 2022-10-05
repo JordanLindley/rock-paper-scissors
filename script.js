d3da021fb9a62de398db122f2e0a113187fa8f5a
JordanLindley <jlindley94@gmail.com>
2022-10-04 21:45:10 -0400

    Create UI and refactor javascript to manipulate the DOM
    Removed console.logs, added event listeners, and displayed results
    directly to the DOM instead of to the terminal.

--- script.js
+++ script.js
@@ -1,72 +1,51 @@
+// begin to count scores.
+let roundsWon = 0;
+let roundsLost = 0;
 
-// getComputerChoice: 
-  // 1. create function getComputerChoice to randomly return either 'Rock', 'Paper', or 'Scissors'.
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const scores = document.querySelector('.scores');
+const results = document.querySelector('.results');
+
+// add event listener to each button, run playRound() on each click.
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', function () {
+  playRound(button.className, getComputerChoice())
+}))
+
+//  randomly return either 'rock', 'paper', or 'scissors'
 function getComputerChoice() {
   const computerSelections = ['rock', 'paper', 'scissors'];
   let random = Math.floor(Math.random()*(2-0+1)+0);
   return computerSelections[random];
 }
-  // 2. Assign returned value of getComputerChoice() to variable computerChoice.
 
-  // getPlayerChoice:
-let playerChoice = 'rock';
-let computerChoice;
-let roundsWon = 0;
-let roundsLost = 0;
-// playRound:
-  // Takes two parameters: computerChoice and playerChoice
-  // Conditional statements. 
-    // `You lose! ${} beats ${}!`
-    // `You win! ${} beats ${}!`
-  // Return results, do not console.log them.
 function playRound(playerChoice, computerChoice) {
+  // winning/losing messages
+  const youWin = `You win! ${capitalizeFirstLetter(playerChoice)} beats ${computerChoice}.`;
+  const youLose = `You lose! ${capitalizeFirstLetter(computerChoice)} beats ${playerChoice}.`;
+  const tie = 'It\'s a tie! Play again!'
 
-  playerChoice = prompt('Rock, Paper, Scissors, Shoot!', playerChoice);
-  computerChoice = getComputerChoice();
-
-  let youWin = `You win! ${playerChoice} beats ${computerChoice}.`;
-  let youLose = `You lose! ${computerChoice} beats ${playerChoice}`;
-  let tie = 'It\'s a tie! Play again!'
-
-  // Must be case insensitive
   if (
-    (playerChoice.toLowerCase() == 'rock' && computerChoice == 'scissors') 
-    || (playerChoice.toLowerCase() == 'scissors' && computerChoice == 'paper') 
-    || (playerChoice.toLowerCase() == 'paper' && computerChoice == 'rock')) {
+    (playerChoice == 'rock' && computerChoice == 'scissors') 
+    || (playerChoice == 'scissors' && computerChoice == 'paper') 
+    || (playerChoice == 'paper' && computerChoice == 'rock')) {
+      // increase Player Score
     roundsWon = ++roundsWon;
-    console.log(`Player Choice: ${playerChoice}`);
-    console.log(`Computer Choice: ${computerChoice}`);
-    console.log(youWin);
+    results.textContent = youWin;
     } 
-   else if (playerChoice.toLowerCase() == computerChoice) {
-    console.log(`Player Choice: ${playerChoice}`);
-    console.log(`Computer Choice: ${computerChoice}`);
-    console.log(tie);
-  } else if (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors' || playerChoice.toLowerCase() == null) {
-    console.log('You must select "rock", "paper", or "scissors"');
-  } 
-  else {
+   else if (playerChoice == computerChoice) {
+    results.textContent = tie;
+  } else {
     roundsLost = ++roundsLost;
-    console.log(`Player Choice: ${playerChoice}`);
-    console.log(`Computer Choice: ${computerChoice}`);
-    console.log(youLose);
+    results.textContent = youLose;
   }
-}
 
-// game() function = 5 rounds.
-  // use a for loop where i = number of rounds.
-  // count roundsWon and roundsLost. Print winning or losing message to the console depending on the outcome.
-function game() {
-  for (i = 1; i < 6; i++) {
-    playRound()
-    console.log(`Round ${i} out of 5!`);
-    console.log(`Player Score: ${roundsWon}. Computer Score: ${roundsLost}`);
-  }
-  if (roundsWon > roundsLost) {
-    console.log('Congratulations! You\'ve won!');
-  } else if (roundsWon == roundsLost) {
-    console.log('It\'s a tie! Out of five. I guess.');
-  } else console.log('Better luck next time!');
+  if (roundsWon >= 5) {
+    scores.textContent = `YOU WIN! Refresh the page to try again! Player Score: ${roundsWon}. Computer Score: ${roundsLost}`;
+  } else if (roundsLost >= 5) {
+    scores.textContent = `Better luck next time! Refresh the page to try again! Player Score: ${roundsWon}. Computer Score: ${roundsLost}`;
+  } else scores.textContent = `Player Score: ${roundsWon}. Computer Score: ${roundsLost}`;
 }
-
-game();
